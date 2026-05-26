cc944fc073dd25eca304aac19f6ab903bd3b61e1
jodamco <jodamco@mail.com>
2026-05-25 21:38:02 -0300

    variables

---
+++ variables/variables.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var a = "initial"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	var strin string
+	fmt.Println(strin)
+
+	var doub float64
+	fmt.Println(doub)
+
+	var boo bool
+	fmt.Println(boo)
+
+	f := "apple"
+	fmt.Println(f)
+}
