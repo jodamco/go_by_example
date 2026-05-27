e0c8a965a006ee3710d57e546b6d2bdf710274c9
jodamco <jodamco@mail.com>
2026-05-27 10:20:46 -0300

    multiple_return

---
+++ multiple_return/multiple_return.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+// Signature to return multple values
+func vals() (int, int) {
+	return 3, 7
+}
+
+func main() {
+	a, b := vals()
+	fmt.Println(a)
+	fmt.Println(b)
+
+	_, c := vals()
+	fmt.Println(c)
+}
